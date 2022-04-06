460f99302ee51b7cbe9296bc6c7988e47c1b7bab
YSOFT\seckar <David.Seckar@ysoft.com>
2022-04-06 16:53:34 +0200

    created getvolumecredits method

--- csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -17,10 +17,7 @@ namespace TheatricalPlayersRefactoringKata
             {
                 var play = plays[perf.PlayID];
                 int performanceAmount = GetPerformanceAmount(perf, play.Type);
-                // add volume credits
-                volumeCredits += Math.Max(perf.Audience - 30, 0);
-                // add extra credit for every ten comedy attendees
-                if (PlayType.Comedy == play.Type) volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
+                volumeCredits = GetVolumeCredits(volumeCredits, perf, play.Type);
 
                 // print line for this order
                 result += string.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(performanceAmount / 100), perf.Audience);
@@ -29,6 +26,14 @@ namespace TheatricalPlayersRefactoringKata
             result += string.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
             result += string.Format("You earned {0} credits\n", volumeCredits);
             return result;
+        }
+
+        private static int GetVolumeCredits(int volumeCredits, Performance perf, PlayType playType) {
+            volumeCredits += Math.Max(perf.Audience - 30, 0);
+            if (PlayType.Comedy == playType)
+                volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
+
+            return volumeCredits;
         }
 
         private static int GetPerformanceAmount(Performance perf, PlayType playType)
